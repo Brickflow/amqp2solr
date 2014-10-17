b08aa3cfec239d102aab8a73c18af19ec90a7cc6
Laci Szűcs <ifrozen@gmail.com>
2014-10-17 23:53:21 +0200

    lib/index.js wraps the two factories into yet another factory,  which returns { getResource(options), getQueue([queueName,] resourceOrOptions: }.

--- run.js
+++ run.js
@@ -1,9 +1,10 @@
-'use strict';
 var _ = require('lodash');
-var config = require('./lib/config');
-var amqp2solr = require('./lib/amqp2solr')({ config: config });
+var amqp2solr = require('./lib')({
+  config: require('./lib/config'),
+  logger: require('./lib/consoleLogger')
+});
 
-var blogResource = amqp2solr.getResource({
+var blogQueue = amqp2solr.getQueue({
   core: 'blogs',
   fields: {
     tumblrUsername: 'id',
@@ -27,46 +28,9 @@ var blogResource = amqp2solr.getResource({
     }
   },
   mlt: {
-    fl: "hashtags_ss"
+    fl: "hashtags_ss",
+    count: 100
   }
 });
-
-//blogResource.solr.commit();
-//blogResource.findAndModify({ id: 'ifroz' }, { foskazal_i: 5 }, function() {
-//  console.log('VISSZAJOTT ANYAD IS', arguments);
-//});
-
-
-amqp2solr.findAndModify('solr-blog', 'ifroz', blogResource.encode({
-  updatedAt: new Date(),
-  akarmi_ss: ['loafszkarikaBhazzzegTeFOSKAZAL'],
-//  images: [
-//    "http://36.media.tumblr.com/f68bdd0321ded9e83b1aa18ba7a403d5/tumblr_nb2iorwmS21qzbl5oo1_400.jpg",
-//    "http://36.media.tumblr.com/dc2fa235f4398b2c12ae8fbd91c65903/tumblr_nb19nobi0b1rhavdko1_250.png",
-//    "http://38.media.tumblr.com/814624703aecc97e7326b7fea2c62849/tumblr_ncyxlm3Idw1rgygrxo2_250.gif",
-//    "http://36.media.tumblr.com/0c9af6027d5f1a4b42bc65807b1a4f86/tumblr_mh06k2VPSd1qahheuo1_r1_400.jpg",
-//    "http://33.media.tumblr.com/fdfcedebe6a3f1d41f0ea9d888770017/tumblr_nc9kuxzDKg1r7ealro1_400.jpg",
-//    "http://38.media.tumblr.com/c7ca75501b9314b294ea5f869a93f585/tumblr_nbsvqiNDLC1qz8q0ho1_250.gif",
-//    "http://40.media.tumblr.com/1127b0552c282fe4eeb9905dbc410131/tumblr_nbwu79kiJ91r7ealro2_r3_400.jpg",
-//    "http://40.media.tumblr.com/fe5e2386a7bc3e624c107fe5718368ca/tumblr_mt5eq3RSsH1r7ealro1_r1_400.jpg",
-//    "http://38.media.tumblr.com/9e5868d551c81e4a51fcb2b970a34c25/tumblr_ncdxkieikS1r7ealro1_400.gif"
-//  ]
-  hashtags: [{ label: 'milf' }, { label: 'porn' }, {label: 'booobies'}, {label: 'bitches'}]
-}));
-
-amqp2solr.listen('solr-blog', blogResource, function(err, queue) {
-  console.log('solr-q-mock', err, queue);
-});
-//
-
-//blogResource.recommend({tumblrUsername: 'ifroz'}, function() {
-//  console.log('recommend', arguments);
-//});
-//
-//blogResource.find({tumblrUsername: 'ifroz'}, function(err, res) {
-//  console.log('MI SELF', err, res);
-//});
-
-//blogResource.solr.commit(function(err, res) {
-//  console.log(err, res);
-//});
+blogQueue.listen();
+blogQueue.createOrUpdate({id: 'phossquazzahl'}, { phosskazal_i:  10 });
